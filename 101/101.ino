97fcd9df88c2102bbc15dce5d2f039dcf66f446b
Your Name <tomi.stenbacka@gmail.com>
2017-01-27 09:51:44 +0200

    Acceleration data added to buffer

--- 101/101.ino
+++ 101/101.ino
@@ -1,5 +1,7 @@
 #include "CurieIMU.h"
 
+float gx, gy, gz; //scaled Gyro values
+
 void setup() {
   Serial.begin(9600); // initialize Serial communication
   while (!Serial);    // wait for the serial port to open
@@ -11,9 +13,15 @@ void setup() {
 }
 
 void loop() {
-  float gx, gy, gz; //scaled Gyro values
-
   // read gyro measurements from device, scaled to the configured range
   CurieIMU.readGyroScaled(gx, gy, gz);
+  
+  char *buffer;
+  buffer = (char*) malloc(80);
+  
+  sprintf(buffer, "%f : %f : %f", gx, gy, gz);
 
+  Serial.println(buffer);
+  delay(100);
+  free (buffer);
 }
