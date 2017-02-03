b25ec1c297049f15374c95b4b8f84919d009b768
Your Name <tomi.stenbacka@gmail.com>
2017-02-03 09:03:43 +0200

    Added BL stuff

--- 101/101.ino
+++ 101/101.ino
@@ -1,10 +1,22 @@
 #include "CurieIMU.h"
+#include <SoftwareSerial.h>
+
+//uncomment for serial messages
+//#define SERIAL_MSG
+
+#define BL_TX 2
+#define BL_RX 3
 
 float gx, gy, gz; //scaled Gyro values
 
+SoftwareSerial bluetooth(BL_TX, BL_RX);
+
 void setup() {
+  bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
+
+  #ifdef SERIAL_MSG
   Serial.begin(9600); // initialize Serial communication
-  while (!Serial);    // wait for the serial port to open
+  #endif
 
   CurieIMU.begin();
 
@@ -19,9 +31,13 @@ void loop() {
   char *buffer;
   buffer = (char*) malloc(80);
   
-  sprintf(buffer, "%f : %f : %f", gx, gy, gz);
+  sprintf(buffer, "%f:%f:%f", gx, gy, gz);
 
+  #ifdef SERIAL_MSG
   Serial.println(buffer);
+  #endif
+  
+  bluetooth.println(buffer);
   delay(100);
   free (buffer);
 }
